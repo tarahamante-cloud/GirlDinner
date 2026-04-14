04533459667e3f072fe6047431baace1b7f3cde0
tarahamante-cloud <tarahamante@gmail.com>
2026-04-14 13:41:51 -0400

    Create GirlDinner component

---
+++ girl-dinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GirlDinner = () => {
+    return (
+        <div>
+            <h1>Girl Dinner</h1>
+            <p>This is my special girl dinner!</p>
+        </div>
+    );
+};
+
+export default GirlDinner;
